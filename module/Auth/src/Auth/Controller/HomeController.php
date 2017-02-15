f2a92e76c843e562885538e29c860a95c9e49765
tranvanhop <tranvanhopcntt@gmail.com>
2017-02-15 21:05:10 +0700

    update

--- module/Auth/src/Auth/Controller/HomeController.php
+++ module/Auth/src/Auth/Controller/HomeController.php
@@ -21,65 +21,17 @@ class HomeController extends BaseController
         return $this->_view;
     }
 
-    public function getMyFriendAction()
+    public function getCaoAnhPhuongAction()
     {
         $this->init();
         $this->_view->setTerminal(true);
 
         if ($this->_request->isGet())
         {
-            $friends = $this->_commonDAO->executeQuery('MY_FRIEND_GET_ALL', array());
-            $this->_view->setVariable('friends', $friends);
+
         }
 
-        $this->_view->setTemplate('application/home/template/my-friend.phtml');
-        $this->writeLog();
-        return $this->_view;
-    }
-
-    public function getSkillsAction()
-    {
-        $this->init();
-        $this->_view->setTerminal(true);
-
-        if ($this->_request->isGet())
-        {
-            $skills = $this->_commonDAO->executeQuery('SKILLS_GET_ALL', array());
-            $this->_view->setVariable('skills', $skills);
-        }
-
-        $this->_view->setTemplate('application/home/template/skills.phtml');
-        $this->writeLog();
-        return $this->_view;
-    }
-
-    public function getPostsLatestAction()
-    {
-        $this->init();
-        $this->_view->setTerminal(true);
-
-        if($this->_request->isGet())
-        {
-            $dateTimeQuery = $this->_utility->getDateTimeQuery('', '');
-            $search = array(
-                (int) $this->params()->fromQuery('id', 0),
-                (int) $this->params()->fromQuery('category_id', 0),
-                $this->params()->fromQuery('title', ''),
-                $this->params()->fromQuery('summary', ''),
-                $this->params()->fromQuery('description', ''),
-                $this->params()->fromQuery('thumbnail', ''),
-                $this->params()->fromQuery('start_date', $dateTimeQuery['start_date']),
-                $this->params()->fromQuery('end_date', $dateTimeQuery['end_date']),
-                (int) $this->params()->fromQuery('create_by', 1),
-                $this->params()->fromQuery('tags', ''),
-                (int) $this->params()->fromQuery('limit', Define::LIMIT_HOME),
-                (int) $this->params()->fromQuery('offset', 0)
-            );
-
-            $posts = $this->_commonDAO->executeQuery('POST_SEARCH', $search);
-            $this->_view->setVariable('posts', $posts);
-        }
-        $this->_view->setTemplate('application/home/template/posts-latest.phtml');
+        $this->_view->setTemplate('auth/home/template/cao-anh-phuong.phtml');
         $this->writeLog();
         return $this->_view;
     }
--- public/js/home.js
+++ public/js/home.js
@@ -1,29 +1,9 @@
-function getMyFriend()
+function getCaoAnhPhuong()
 {
-    var url = url_base + '/home/getMyFriend';
+    var url = url_base + '/home/getCaoAnhPhuong?token=';
 
     $.get(url, {}, function(data)
     {
-        $('#my-friend').html(data);
-    });
-}
-
-function getPostsLatest()
-{
-    var url = url_base + '/home/getPostsLatest';
-
-    $.get(url, {}, function(data)
-    {
-        $('#posts-latest').html(data);
-    });
-}
-
-function getSkills()
-{
-    var url = url_base + '/home/getSkills';
-
-    $.get(url, {}, function(data)
-    {
-        $('#skills').html(data);
+        $('#cao-anh-phuong').html(data);
     });
 }
